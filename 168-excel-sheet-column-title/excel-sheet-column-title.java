60e02f9281001a831fd0cc89f874780a0133110e
Naman Chaudhary <namanvit2028@gmail.com>
2026-06-18 21:46:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (75.88%) - LeetSync

---
+++ 168-excel-sheet-column-title/excel-sheet-column-title.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        StringBuilder sb = new StringBuilder();
+
+        while (columnNumber > 0) {
+            columnNumber--; // adjust for 1-based indexing
+
+            char ch = (char) ('A' + (columnNumber % 26));
+            sb.append(ch);
+
+            columnNumber /= 26;
+        }
+
+        return sb.reverse().toString();
+    }
+}
