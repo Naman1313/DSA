6a6273a69b00a442dc77da012e90ae244b906862
Naman Chaudhary <namanvit2028@gmail.com>
2026-06-22 22:27:38 +0530

    Time: 4 ms (38.82%) | Memory: 43.4 MB (40.49%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            // If opening bracket, push into stack
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } 
+            // If closing bracket, check top of stack
+            else {
+                if (stack.isEmpty()) return false;
+
+                char top = stack.pop();
+
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        // If stack is empty, all brackets matched
+        return stack.isEmpty();
+    }
+}
